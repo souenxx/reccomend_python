0b806418669b0b363ab38768cef3078f5bd9ecc7
souenxx <souen_xx@yahoo.co.jp>
2019-07-08 13:33:29 +0900

    modified

--- data_preprocess/file_ndcg.py
+++ data_preprocess/file_ndcg.py
@@ -4,14 +4,19 @@ file = "C:/Users/kidos/git/reccomend_python/data_preprocess/test2.txt"
 file2 = "C:/Users/kidos/git/reccomend_python/data_preprocess/test2c.txt"
 
 def mydict(path):
-    user_dict=[]
+    user_dict={}
+    feature_list=[]
     with open(path) as data_file:
         for line in data_file:
             s=list(line.rstrip().split(","))
             user_id = s[0]
+            pair={}
+            pair['feature']=s[1]
+            pair['score']=s[2]
             if user_id not in user_dict:
-                user_dict.append(user_id)
-            #user_dict[user_id][(s[0],s[1])] = s[2] 
+                user_dict[user_id]=[]
+            user_dict[user_id].append(pair)
+            #user_dict[user_id].append(pair)
     return user_dict
 
 def get_ndcg(list1,list2):
@@ -35,23 +40,33 @@ def get_dcg(rel,rank):
     else:
         dcg=rel/(np.log2(rank))
     return dcg
-    
 
-
+def get_featurelist(n,user_id):
+    featurelist=[]
+    for i in range(len(n[user_id])):
+        featurelist.append(n[user_id][i]['feature'])
+    return featurelist
 
 n=mydict(file)
+nc=mydict(file)
 n2=mydict(file2)
 
-
-print(n)
-#for mykey in n.keys():
-    #if mykey in n2:
-        #print(mykey)
-        #n2[mykey]=n[mykey]
-    #else:
-        #n2[mykey]=0
+for user_id in n.keys():
+    for i in range(len(n[user_id])):
+        flist=get_featurelist(n,user_id)
+        flist2=get_featurelist(n2,user_id)
+        name_values = [x['feature'] for x in n2[user_id] if x['feature'] == flist[i]]
+        if len(name_values):
+            nc[user_id][i]['score']=n2[user_id][flist2.index(name_values[0])]['score']
+        else:
+            nc[user_id][i]['score']=0
+    print(nc)
+            
+#print(n2['49'])
 
 #nl=list(n.values())
+
+#print(nl)
 #nlf=[float(s) for s in nl]
 
 #nl2=list(n2.values())
